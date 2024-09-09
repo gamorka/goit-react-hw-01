1b6f662499fcf669155f1d3608865f9793866397
gamorka <gamorkaaa@gmail.com>
2024-09-09 08:05:37 +0200

    start

---
+++ react-1/src/App.jsx
@@ -0,0 +1,7 @@
+import Profile from "./components/userData/profile"; 
+const App = () => {
+  return <div>
+    <Profile name = "Jacques Gluke" tag = "@" />
+  </div>
+}
+export default App;
---
+++ react-1/src/components/profile/profile.jsx
@@ -0,0 +1,14 @@
+const Profile = ({name, tag, location, image, stats}) => {
+  console.log(props)
+    return (<>
+      <p>{name}</p>;
+      <p>{tag}</p>;
+      <p>{location}</p>;
+      <p>{image}</p>;
+      <p>{stats}</p>;
+    </>
+    )
+
+}
+
+export default Profile
