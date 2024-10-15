a406e8df1a8b8ac6d9a3eb66c127d65975d7c90a
gamorka <gamorkaaa@gmail.com>
2024-10-15 21:19:09 +0200

    styles for profile

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -1,28 +1,30 @@
+import css from './profile.module.css';
+
 const Profile = () => {
   return (
-      <div>
- <div>
- <img
+      <div className={css.card}>
+ <div className={css.wrapper}>
+ <img className={css.picture}
  src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
  alt="User avatar"
  />
- <p>Petra Marica</p>
- <p>@pmarica</p>
- <p>Salvador, Brasil</p>
+ <p className={css.username}>Petra Marica</p>
+ <p className={css.nickname}>@pmarica</p>
+ <p className={css.location}>Salvador, Brasil</p>
  </div>
 
- <ul>
- <li>
- <span>Followers</span>
- <span>1000</span>
+ <ul className={css.list}>
+ <li className={css.item}>
+ <span className={css.itemTitle}>Followers</span>
+ <span className={css.number}>1000</span>
  </li>
- <li>
- <span>Views</span>
- <span>2000</span>
+ <li className={css.item}>
+ <span className={css.itemTitle} >Views</span>
+ <span className={css.number}>2000</span>
  </li>
- <li>
- <span>Likes</span>
- <span>3000</span>
+ <li className={css.item}>
+ <span className={css.itemTitle}>Likes</span>
+ <span className={css.number}>3000</span>
  </li>
  </ul>
 </div>
